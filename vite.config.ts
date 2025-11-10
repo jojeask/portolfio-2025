148a4e09171ab60323cba2e98348e46f16b24155
Johan Asklund <jojeask@gmail.com>
2025-11-10 11:26:33 +0100

    Revert all changes made on 2025-11-10

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/portolfio-2025/',
   server: {
     host: "::",
     port: 8080,

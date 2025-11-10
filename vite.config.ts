c0489fed5a5b47b7eeaaf23e24f1ad3c6c2b7325
Johan Asklund <jojeask@gmail.com>
2025-11-10 11:48:18 +0100

    New build. vite.confi now has a base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/portolfio-2025/',
   server: {
     host: "::",
     port: 8080,
